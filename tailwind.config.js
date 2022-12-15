597fe316f724435f428e9b6b5d4f282fa4ac2886
Luis Yps Fdez <lueryefe@gmail.com>
2022-12-15 12:13:56 -0500

    Adding mobile responsive menu

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./docs/**/*.{html,js}"],
+  content: ["./docs/**/*.{html,css,js}"],
   theme: {
     extend: {
       backgroundImage: {
